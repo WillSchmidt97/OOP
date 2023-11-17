eb1b8c753de3c129e6cb6504d965651dad062988
WillSchmidt97 <willschmidt1997@gmail.com>
2023-11-16 22:08:16 -0300

    added a method to calculate a new balance in the savings Account class

--- BankAccount/src/SavingsAccount.java
+++ BankAccount/src/SavingsAccount.java
@@ -6,4 +6,16 @@ public class SavingsAccount extends Account {
         super(client, accountNumber, balance);
         dailyIncome = dIncome;
     }
+
+    public void applyInterest(BigDecimal rate) {
+        if (rate.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("The rate number has to be a positive number and greater than 0.");
+        }
+
+        BigDecimal multiplier = rate.divide(BigDecimal.valueOf(100));
+        BigDecimal r = balance.multiply(multiplier);
+        balance = balance.add(r);
+
+        System.out.println("rate balance: " + r + ". New balance: " + balance);
+    }
 }
